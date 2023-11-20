01386eff392cdaaa83a0ce908b9f836940f37273
VinMacri <114018421+VinMacri@users.noreply.github.com>
2023-11-20 15:51:03 -0500

    Airplane

--- Airplane.java
+++ Airplane.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Airplane {
 String[][] firstClass = new String[5][4];
@@ -6,18 +7,120 @@ String[][] economy = new String[16][6];
 	public Airplane() {
 		for(int i = 0; i < firstClass.length; i++) {
 			for(int x = 0; x < firstClass[i].length; x++) {
-				firstClass[i][x] = "x";
+				firstClass[i][x] = "O";
 			}
 		}
 		
 		for(int i = 0; i < economy.length; i++) {
 			for(int x = 0; x < economy[i].length; x++) {
-				economy[i][x] = "x";
+				economy[i][x] = "O";
 			}
 		}
 	
 	}
-	public void addSeat() {
+	public void addSeat(String clas, int pass, String spot) {
+		if(clas.equals("first")) {
+			if(pass == 1) {
+				if(spot.equals("w")) {
+					boolean placedSeat = false;
+					for(int i = 0; i < firstClass.length; i++) {
+						while(!placedSeat) {
+							for(int x = 0; x < firstClass[i].length; x++) {
+								if(x == 0 || x == 3) {
+									if(firstClass[i][x].equals("O")) {
+										firstClass[i][x] = "x";
+										placedSeat = true;
+										break;
+									}
+								}
+							}
+						}
+					}
+				}
+				else if(spot.equals("a")) {
+					boolean placedSeat = false;
+					for(int i = 0; i < firstClass.length; i++) {
+						while(!placedSeat) {
+							for(int x = 0; x < firstClass[i].length; x++) {
+								if(x == 1 || x == 2) {
+									if(firstClass[i][x].equals("O")) {
+										firstClass[i][x] = "x";
+										placedSeat = true;
+										break;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			else if(pass == 2) {
+				boolean placedSeat = false;
+				for(int i = 0; i < firstClass.length; i++) {
+					while(!placedSeat) {
+						for(int x = 0; x < firstClass[i].length; x++) {
+							if(firstClass[i][x].equals("O") && firstClass[i][x+1].equals("O")) {
+								firstClass[i][x] = "x";
+								firstClass[i][x+1] = "x";
+								placedSeat = true;
+								break;
+							}
+						}
+					}
+				}
+			}
+		}
+		else if(clas.equals("economy")) {
+			if(spot.equals("w")) {
+				boolean placedSeat = false;
+				for(int i = 0; i < economy.length; i++) {
+					while(!placedSeat) {
+						for(int x = 0; x < economy[i].length; x++) {
+							if(x == 0 || x == 5) {
+								if(economy[i][x].equals("O")) {
+									economy[i][x] = "x";
+									placedSeat = true;
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+			else if(spot.equals("a")) {
+				boolean placedSeat = false;
+				for(int i = 0; i < economy.length; i++) {
+					while(!placedSeat) {
+						for(int x = 0; x < economy[i].length; x++) {
+							if(x == 2 || x == 3) {
+								if(economy[i][x].equals("O")) {
+									economy[i][x] = "x";
+									placedSeat = true;
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+			else if(spot.equals("c")) {
+				boolean placedSeat = false;
+				for(int i = 0; i < economy.length; i++) {
+					while(!placedSeat) {
+						for(int x = 0; x < economy[i].length; x++) {
+							if(x == 1 || x == 4) {
+								if(economy[i][x].equals("O")) {
+									economy[i][x] = "x";
+									placedSeat = true;
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
 	}
 	public String showSeating(){
 		String seat = "";
