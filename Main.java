98063509f10e195371a669cdb8df80f1b5415080
VinMacri <114018421+VinMacri@users.noreply.github.com>
2023-11-20 15:50:51 -0500

    main

--- Main.java
+++ Main.java
@@ -8,14 +8,86 @@ public class Main {
 		boolean run = true;
 		while(run) {
 			System.out.println("A)dd S)how Q)uit");
-			String input = in.nextLine();
-			if(input.equalsIgnoreCase("a")){
-				a.addSeat();
+			String choice = in.nextLine();
+			if(choice.equalsIgnoreCase("a")){
+				String clas = "";
+				int pass = 0;
+				String spot = "";
+				boolean valid = false;
+				while(!valid) {
+					System.out.println("F)irst or E)conomy: ");
+					String input = in.next();
+					if(input.equalsIgnoreCase("f")) {
+						clas = "first";
+						boolean validPass = false;
+						while(!validPass) {
+							System.out.println("Passengers? (1-2): ");
+							pass = Integer.parseInt(in.next());
+							if(pass == 2) {
+								validPass = true;
+							}
+							else if(pass == 1) {
+								validPass = true;
+								boolean validSpot = false;
+								while(!validSpot) {
+									System.out.println("A)isle or W)indow: ");
+									spot = in.next();
+									if(spot.equalsIgnoreCase("a")||spot.equalsIgnoreCase("w")) {
+										validSpot = true;
+									}
+									else {
+										System.out.println("Invalid Choice. Enter Again: ");
+									}
+									
+								}
+							}
+							else {
+								System.out.println("Invalid input");
+							}
+						}
+						valid = true;
+					}
+					else if(input.equalsIgnoreCase("e")) {
+						boolean validPass = false;
+						clas = "economy";
+						System.out.println("Passengers? (1-3): ");
+						while(!validPass) {
+							pass = Integer.parseInt(in.next());
+							if(pass == 3) {
+								validPass = true;
+							}
+								
+							else if(pass == 1 || pass == 2) {
+								validPass = true;
+								boolean validSpot = false;
+								while(!validSpot) {
+									System.out.println("A)isle or W)indow: ");
+									spot = in.next();
+									if(spot.equalsIgnoreCase("a")||spot.equalsIgnoreCase("w")) {
+										validSpot = true;
+									}
+									else {
+										System.out.println("Invalid Choice. Enter Again: ");
+									}
+									
+								}
+							}
+							else {
+								System.out.println("Invalid input");
+							}
+						}
+						valid = true;
+					}
+					else {
+						System.out.println("Enter a valid input");
+					}
+				}
+				a.addSeat(clas,pass,spot);
 			}
-			else if(input.equalsIgnoreCase("s")){
+			else if(choice.equalsIgnoreCase("s")){
 				System.out.println(a.showSeating());
 			}
-			else if(input.equalsIgnoreCase("q")) {
+			else if(choice.equalsIgnoreCase("q")) {
 				run = false;
 			}
 		}
